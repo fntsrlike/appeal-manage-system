21deb48b6c4cf74b1ae2b94f268194000e4acf3b
Ruoshi.Ling <fntsrlike@gmail.com>
2014-04-07 19:06:05 +0800

    新增資料庫Schema

---
+++ app/database/migrations/2014_04_06_111847_create_appeal_table.php
@@ -0,0 +1,91 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppealTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appeal_manager', function($table)
+        {
+            $table->increments('m_id');
+            $table->integer('u_id')->unsigned()->unique();
+            $table->string('m_name');
+            $table->string('m_title');
+            $table->string('m_email');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('appeal_complainant', function($table)
+        {
+            $table->increments('c_id');
+            $table->integer('u_id')->unsigned()->unique();
+            $table->string('c_name');
+            $table->integer('c_number');
+            $table->string('c_department');
+            $table->integer('c_grade');
+            $table->string('c_phone');
+            $table->string('c_email');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('appeal_form', function($table)
+        {
+            $table->increments('f_id');
+            $table->integer('c_id')->unsigned();
+            $table->string('f_title');
+            $table->date('f_date');
+            $table->string('f_place');
+            $table->string('f_target');
+            $table->text('f_content');
+            $table->text('f_report');
+            $table->integer('f_status');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('appeal_reply', function($table)
+        {
+            $table->increments('r_id');
+            $table->integer('f_id')->unsigned();
+            $table->string('r_type');
+            $table->text('r_content');
+            $table->integer('r_u_id')->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('appeal_user', function($table)
+        {
+            $table->increments('u_id');
+            $table->string('username')->unique();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $prefix = 'bak_';
+        $suffix = '_' . date('YmdHis');
+
+        Schema::rename('appeal_complainant', $prefix . 'appeal_complainant' . $suffix);
+        Schema::rename('appeal_form', $prefix . 'appeal_form' . $suffix);
+        Schema::rename('', $prefix . '' . $suffix);
+        Schema::rename('', $prefix . '' . $suffix);
+    }
+
+}
