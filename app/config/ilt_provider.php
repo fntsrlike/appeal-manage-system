1c0a1986215ce326f4bcee1cd408b2eca72acb04
Ruoshi.Ling <fntsrlike@gmail.com>
2014-05-05 03:17:16 +0800

    更新設定檔資料。

--- app/config/ilt_provider.php
+++ app/config/ilt_provider.php
@@ -2,6 +2,6 @@
 return array(
     'key'        => 'KEY',
     'secret'     => 'SECRET',
-    'host_url'   => 'https://foo.com/bar/oauth',
-    'scope'      => 'SCOPE',
+    'host_url'   => 'https://domain/oauth',
+    'scope'      => 'user.login.basic+user.login.student+user.info.basic+user.info.student+user.info.personal',
 );
--- app/config/site.php
+++ app/config/site.php
@@ -1,9 +1,10 @@
 <?php
 
 return array(
+    'name' => '權益申訴系統',
     'copyright' => array(
         'creator'   => 'Fntsrlike',
         'maintaner' => 'Fntsrlike',
         'copyright' => 'Fntsrlike'
-    );
+    )
 );
